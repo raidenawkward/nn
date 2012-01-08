43cf8c341110849da2fde0af4112df0f5733384b
Raiden Awkward <raiden@raiden-gentoo.(none)>
2012-01-08 17:24:11 +0800

    methods of BPNode implemented
    struct of BPNode redefined

--- bp/bp_node.c
+++ bp/bp_node.c
@@ -1,1 +1,49 @@
 #include "bp_node.h"
+#include <stdlib.h>
+
+struct BPNode* bp_node_create() {
+	struct BPNode *node = (struct BPNode*)malloc(sizeof(struct BPNode));
+	if (!node)
+		return node;
+
+	node->input_weights = NULL;
+	node->input_count = 0;
+	node->threshold = 0;
+	node->output = 0;
+	node->next = NULL;
+
+	return node;
+}
+
+struct BPNode* bp_node_init(float *inputWeight, int inputCount, float threshold, float output) {
+
+	struct BPNode *node = (struct BPNode*)malloc(sizeof(struct BPNode));
+	if (!node)
+		return node;
+
+	node->input_weights = (float*)malloc(sizeof(float) * inputCount);
+	int i;
+	for (i = 0; i < inputCount; ++i) {
+		node->input_weights[i] = inputWeight[i];
+	}
+
+	node->input_count = inputCount;
+	node->threshold = threshold;
+	node->output = output;
+	node->next = NULL;
+
+	return node;
+
+}
+
+void bp_node_destory(struct BPNode* node) {
+	if (!node)
+		return;
+
+	if (node->input_weights)
+		free(node->input_weights);
+
+	free(node);
+	node = NULL;
+}
+
--- bp/bp_node.h
+++ bp/bp_node.h
@@ -3,13 +3,18 @@
 
 struct BPNode {
 	float* input_weights;
-	int	intput_length;
+	int	input_count;
 	float threshold;
 	float output;
+	// may need function entry for getting real output
+	//
+	struct BPNode *next;
 };
 
-
+/// empty node created, returns NULL if failed
 struct BPNode* bp_node_create();
+/// node with inited values will be created, returns NULL if failed
+struct BPNode* bp_node_init(float *inputWeight, int inputCount, float threshold, float output);
+/// node destory, after doing that node == NULL
 void bp_node_destory(struct BPNode* node);
-
 #endif //_BP_NODE_H
